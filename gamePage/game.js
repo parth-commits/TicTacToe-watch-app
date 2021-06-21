a9cfc3f2436b36a49f6e5e2473965267895c43db
parth-commits <parthpatel1087@gmail.com>
2021-06-21 12:04:24 -0400

    almost done. clean up code now and then done

--- gamePage/game.js
+++ gamePage/game.js
@@ -22,6 +22,12 @@ x.appendChild(xline2);
 const o = document.createElement('div');
 o.classList.add('o');
 
+// strike colors
+let xStrikeColor = "#f2ebd3";
+
+let oStrikeColor = "#545454";
+
+
 // Start shape
 let turn = 'x';
 
@@ -64,11 +70,59 @@ horizontal2.id = 'horizontal-2';
 // the board html
 let boardHTML = document.getElementById('landing-table');
 
+// the strike
+let horizontalStrike1 = document.createElement('div');
+horizontalStrike1.classList.add('board-horizontal-strike-line');
+horizontalStrike1.classList.add('strike');
+horizontalStrike1.id = 'horizontal-strike-1';
+
+let horizontalStrike2 = document.createElement('div');
+horizontalStrike2.classList.add('board-horizontal-strike-line');
+horizontalStrike2.classList.add('strike');
+horizontalStrike2.id = 'horizontal-strike-2';
+
+let horizontalStrike3 = document.createElement('div');
+horizontalStrike3.classList.add('board-horizontal-strike-line');
+horizontalStrike3.classList.add('strike');
+horizontalStrike3.id = 'horizontal-strike-3';
+
+let verticalStrike1 = document.createElement('div');
+verticalStrike1.classList.add('board-vertical-strike-line');
+verticalStrike1.classList.add('strike');
+verticalStrike1.id = 'vertical-strike-1';
+
+let verticalStrike2 = document.createElement('div');
+verticalStrike2.classList.add('board-vertical-strike-line');
+verticalStrike2.classList.add('strike');
+verticalStrike2.id = 'vertical-strike-2';
+
+let verticalStrike3 = document.createElement('div');
+verticalStrike3.classList.add('board-vertical-strike-line');
+verticalStrike3.classList.add('strike');
+verticalStrike3.id = 'vertical-strike-3';
+
+let diagonalStrike1 = document.createElement('div');
+diagonalStrike1.classList.add('board-diagonal-strike-line');
+diagonalStrike1.classList.add('strike');
+diagonalStrike1.id = 'diagonal-strike-1';
+
+let diagonalStrike2 = document.createElement('div');
+diagonalStrike2.classList.add('board-diagonal-strike-line');
+diagonalStrike2.classList.add('strike');
+diagonalStrike2.id = 'diagonal-strike-2';
+
+
+
 /*
-<div class="board-vertical-line line" id="vertical-1"></div>
-<div class="board-vertical-line line" id="vertical-2"></div>
-<div class="board-horizontal-line line" id="horizontal-1"></div>
-<div class="board-horizontal-line line" id="horizontal-2"></div>
+
+          <div class="board-horizontal-strike-line strike" id="horizontal-strike-1"></div>
+          <div class="board-horizontal-strike-line strike" id="horizontal-strike-2"></div>
+          <div class="board-horizontal-strike-line strike" id="horizontal-strike-3"></div>
+          <div class="board-vertical-strike-line strike" id="vertical-strike-1"></div>
+          <div class="board-vertical-strike-line strike" id="vertical-strike-2"></div>
+          <div class="board-vertical-strike-line strike" id="vertical-strike-3"></div>
+          <div class="board-diagonal-strike-line strike" id="diagonal-strike-1"></div>
+          <div class="board-diagonal-strike-line strike" id="diagonal-strike-2"></div>
 
 */
 
@@ -82,9 +136,13 @@ async function clicked(num) {
     if (board[row][col] === '') {
         boxes[num].appendChild(turnElem);
         board[row][col] = turn;
+        await new Promise(r => setTimeout(r, 250));
         if (isWinner(turn)) {
             await new Promise(r => setTimeout(r, 2000));
             increaseScore(turn);
+            await deleteAllInput();
+            await boardAppear();
+        } else if (isboardFilled()) {
             await deleteAllInput();
             await boardAppear();
         } else {
@@ -106,6 +164,12 @@ async function deleteAllInput() {
     gameTable.style.animation = "boardDisappear 0.5s ease-in-out";
     gameTable.style.animationFillMode = "forwards";
     await new Promise(r => setTimeout(r, 600));
+
+    let strikes = document.getElementsByClassName('strike');
+    strikes = Array.prototype.slice.call(strikes);
+    for (let i = 0; i < strikes.length; i++) {
+        strikes[i].remove();
+    }
     let lines = document.getElementsByClassName('line');
     lines = Array.prototype.slice.call(lines);
     for (let i = 0; i < lines.length; i++) {
@@ -125,28 +189,58 @@ async function deleteAllInput() {
 
 // checks if there is a winner, returns true if there is, else false
 function isWinner(turn) {
+    let strikeColor;
+    if (turn === 'x') {
+        strikeColor = xStrikeColor;
+    } else {
+        strikeColor = oStrikeColor;
+    }
     if (board[0][0] === board[0][1] && board[0][0] === board[0][2] && board[0][0] === turn) {
+        let temp = horizontalStrike1.cloneNode(true);
+        temp.style.backgroundColor = strikeColor;
+        boardHTML.appendChild(temp);
         console.log("1");
         return true;
     } else if (board[1][0] === board[1][1] && board[1][0] === board[1][2] && board[1][0] === turn) {
+        let temp = horizontalStrike2.cloneNode(true);
+        temp.style.backgroundColor = strikeColor;
+        boardHTML.appendChild(temp);
         console.log("2");
         return true;
     } else if (board[2][0] === board[2][1] && board[2][0] === board[2][2] && board[2][0] === turn) {
+        let temp = horizontalStrike3.cloneNode(true);
+        temp.style.backgroundColor = strikeColor;
+        boardHTML.appendChild(temp);
         console.log("3");
         return true;
     } else if (board[0][0] === board[1][0] && board[0][0] === board[2][0] && board[0][0] === turn) {
+        let temp = verticalStrike1.cloneNode(true);
+        temp.style.backgroundColor = strikeColor;
+        boardHTML.appendChild(temp);
         console.log("4");
         return true;
     } else if (board[0][1] === board[1][1] && board[0][1] === board[2][1] && board[0][1] === turn) {
+        let temp = verticalStrike2.cloneNode(true);
+        temp.style.backgroundColor = strikeColor;
+        boardHTML.appendChild(temp);
         console.log("5");
         return true;
     } else if (board[0][2] === board[1][2] && board[0][2] === board[2][2] && board[0][2] === turn) {
+        let temp = verticalStrike3.cloneNode(true);
+        temp.style.backgroundColor = strikeColor;
+        boardHTML.appendChild(temp);
         console.log("6");
         return true;
     } else if (board[0][0] === board[1][1] && board[0][0] === board[2][2] && board[0][0] === turn) {
+        let temp = diagonalStrike1.cloneNode(true);
+        temp.style.backgroundColor = strikeColor;
+        boardHTML.appendChild(temp);
         console.log("7");
         return true;
     } else if (board[0][2] === board[1][1] && board[0][2] === board[2][0] && board[0][2] === turn) {
+        let temp = diagonalStrike2.cloneNode(true);
+        temp.style.backgroundColor = strikeColor;
+        boardHTML.appendChild(temp);
         console.log("8");
         return true;
     } else {
@@ -188,4 +282,13 @@ async function boardAppear() {
     document.getElementById('horizontal-1').style.animation = "horizontalAnim 1s ease-out;";
     document.getElementById('horizontal-2').style.animation = "horizontalAnim 1s ease-out;";
     await new Promise(r => setTimeout(r, 1000));
+}
+
+// is board filled
+function isboardFilled() {
+    if (board[0][0] !== '' && board[0][1] !== '' && board[0][2] !== '' && board[1][0] !== '' && board[1][1] !== '' && board[1][2] !== '' && board[2][0] !== '' && board[2][1] !== '' && board[2][2] !== '') {
+        return true;
+    } else {
+        return false;
+    }
 }
